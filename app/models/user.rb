a6db4343fd954630ec7ae209d9a79df214f7927b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-05-31 13:11:28 +0100

    Setup user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { minimun: 6 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimun: 6 }
+end
